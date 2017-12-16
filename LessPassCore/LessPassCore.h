a756e77697b1d754d1803dbaf16ba487f3fb46ee
Romain Quidet <contact@xdappfactory.com>
2017-12-16 11:57:37 +0100

    Initial Commit

---
+++ LessPassCore/LessPassCore.h
@@ -0,0 +1,19 @@
+//
+//  LessPassCore.h
+//  LessPassCore
+//
+//  Created by Romain Quidet on 16/12/2017.
+//  Copyright © 2017 XDAppfactory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LessPassCore.
+FOUNDATION_EXPORT double LessPassCoreVersionNumber;
+
+//! Project version string for LessPassCore.
+FOUNDATION_EXPORT const unsigned char LessPassCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LessPassCore/PublicHeader.h>
+
+
